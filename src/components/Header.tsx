b630c05b7548550f5670920b22941444647049ec
MohammedBetkaoui <mohammed.betkaoui@univ-bba.dz>
2025-01-04 15:40:07 +0100

    modification

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           {/* Logo */}
-          <div className="text-2xl font-bold text-gray-900 dark:text-white">B450M</div>
+          <div className="text-2xl font-bold text-gray-900 dark:text-white">BM</div>
           
           {/* Desktop Navigation */}
           <nav className="hidden md:flex space-x-8">
